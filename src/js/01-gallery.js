53e8f1a5bbe6684757e53c46169c04b0d9d58ea9
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-02-24 20:07:00 +0200

    1st task competed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,16 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryElem = document.querySelector('.gallery');
+const previewImg = galleryItems.reduce((acc, item) => {
+  return (acc += `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+</a>`);
+}, '');
+
+galleryElem.insertAdjacentHTML('beforeend', previewImg);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
