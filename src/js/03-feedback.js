37ebe95737bf90990ae7988b7f26a2a025c6dfbe
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-11-06 21:31:11 +0200

    add ts config

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,37 @@
 import throttle from 'lodash.throttle';
-
 const refs = {
-  input: document.querySelector('input[type=email]'),
-  textarea: document.querySelector('textarea[name=message]'),
-  form: document.querySelector('.feedback-form'),
-  button: document.querySelector('button[type=submit]'),
+    input: document.querySelector('input[type=email]'),
+    textarea: document.querySelector('textarea[name=message]'),
+    form: document.querySelector('.feedback-form'),
+    button: document.querySelector('button[type=submit]'),
 };
-
 const onSubmitAction = event => {
-  event.preventDefault();
-  event.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-  userData = {};
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+    userData = {};
 };
-
 let userData;
-
 if (localStorage.getItem('feedback-form-state')) {
-  const userParceData = JSON.parse(localStorage.getItem('feedback-form-state'));
-  userData = { ...userParceData };
-} else {
-  userData = {};
+    const userParceData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    userData = { ...userParceData };
 }
-
+else {
+    userData = {};
+}
 fillInputs();
-
 const onInputText = event => {
-  userData[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
-  console.log(userData);
+    userData[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+    console.log(userData);
 };
-
 function fillInputs() {
-  if (userData.email) {
-    refs.input.value = userData.email;
-  }
-  if (userData.message) {
-    refs.textarea.value = userData.message;
-  }
+    if (userData.email) {
+        refs.input.value = userData.email;
+    }
+    if (userData.message) {
+        refs.textarea.value = userData.message;
+    }
 }
-
 refs.form.addEventListener('input', throttle(onInputText, 500));
 refs.form.addEventListener('submit', onSubmitAction);
--- src/ts/01-gallery.ts
+++ src/ts/01-gallery.ts
@@ -3,11 +3,14 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const galleryElem = document.querySelector('.gallery');
 const previewImg = galleryItems.reduce((acc, item) => {
-    return (acc += `<a class="gallery__item" href="${item.original}">
+  return (acc += `<a class="gallery__item" href="${item.original}">
   <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
 </a>`);
 }, '');
+
 galleryElem.insertAdjacentHTML('beforeend', previewImg);
+
 const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
