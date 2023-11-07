199d9822c9882ee3e47fba477a980fdc447554a9
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-11-07 19:00:52 +0200

    2nd task in ts

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,10 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
-player.on('timeupdate', throttle(data => {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-}, 1000));
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+if (iframe) {
+    const player = new Player(iframe);
+    player.on('timeupdate', throttle(data => {
+        localStorage.setItem('videoplayer-current-time', data.seconds);
+    }, 1000));
+    player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')) || 0);
+}
--- src/ts/02-video.ts
+++ src/ts/02-video.ts
@@ -1,12 +1,16 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const iframe: HTMLIFrameElement | null = document.querySelector('iframe');
+if (iframe) {
+  const player = new Player(iframe);
 
-player.on(
-  'timeupdate',
-  throttle(data => {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-  }, 1000)
-);
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+  player.on(
+    'timeupdate',
+    throttle(data => {
+      localStorage.setItem('videoplayer-current-time', data.seconds);
+    }, 1000)
+  );
+  player.setCurrentTime(
+    Number(localStorage.getItem('videoplayer-current-time')) || 0
+  );
+}
